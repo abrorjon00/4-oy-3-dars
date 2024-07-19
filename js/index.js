01257806b327bb4662450cd064de88f009492135
abdurahimov0101 <abrorjonabdurahimov42@gmail.com>
2024-07-19 15:20:20 +0300

    3-dars bajarildi

---
+++ js/index.js
@@ -0,0 +1,257 @@
+// // Ifga oid misollar
+// 1-misol
+// let age = +prompt("Yoshingizni kiriting..")
+
+// if(age >= 18) {
+//     console.log("Sizni yoshiniz katta")
+// } else {
+//     console.log("Sizni yoshiniz kichik")
+// }
+
+// 2-misol
+
+// let number = +prompt("Son kiriting..")
+
+// if(number % 2 == 0) {
+//     console.log("Juft son")
+// } else {
+//     console.log("Toq son")
+// }
+
+// 3-misol
+
+// let number = +prompt("Son kiriting..")
+
+// if(number > 0) {
+//     console.log("Musbat son")
+// } else {
+//     console.log("Manfiy son")
+// }
+
+// 4-misol
+
+// let weather = +prompt("Son kiriting..")
+
+// if(weather > 20) {
+//     console.log("Yengil kiyining")
+// } else {
+//     console.log("Qalin kiyining")
+// }
+
+// 5-misol
+
+// let ball = +prompt("Son kiriting..")
+
+// if(ball > 90) {
+//     console.log("Alo")
+// } else if( ball < 89 && ball > 70){
+//     console.log("Yaxshi")
+// } else if( ball < 69) {
+//     console.log("Qoniqarli")
+// } else  {
+//     console.log("Yomon")
+// }
+
+
+// 7-misol
+
+// let money = +prompt("Narxni kiriting..")
+
+// if(money > 100) {
+//     console.log("Chegirma qo`llanildi")
+// } else {
+//     console.log("Chegirma mavjud emas")
+// }
+
+
+
+// Boolean oid misollar
+
+// 11-misol
+
+// let A = +prompt("1-Sonni kiriting..")
+// let B = +prompt("2-Sonni kiriting..")
+
+// let =   res = false;
+
+// if (A % 2 == 1 && B % 2 == 1 || A % 2 == 0 &&  B % 2 == 0 ) {
+//     res = true;
+// }
+
+// console.log(res);
+
+// 12-misol
+
+// let A = +prompt("1-Sonni kiriting..")
+// let B = +prompt("2-Sonni kiriting..")
+// let C = +prompt("3-Sonni kiriting..")
+
+// let =   res = false;
+
+// if (A > 0 && B > 0 && C > 0 ) {
+//     res = true;
+// }
+
+// console.log(res);\
+
+// 13-misol
+
+// let A = +prompt("1-Sonni kiriting..")
+// let B = +prompt("2-Sonni kiriting..")
+// let C = +prompt("3-Sonni kiriting..")
+
+// let =   res = false;
+
+// if (A < 0 && B < 0 && C > 0 || A < 0 && B > 0 && C < 0 || A > 0 && B < 0 && C < 0 ){
+//     res = true;
+// }
+
+// console.log(res)
+
+// 14-misol
+ 
+// let A = +prompt("1-Sonni kiriting..")
+// let B = +prompt("2-Sonni kiriting..")
+// let C = +prompt("3-Sonni kiriting..")
+
+// let =   res = false;
+
+// if (A < 0 && B < 0 && C > 0 || A < 0 && B > 0 && C < 0 || A > 0 && B < 0 && C < 0 ){
+//     res = true;
+// }
+
+// console.log(res)
+
+// 15-misol
+
+// let A = +prompt("1-Sonni kiriting..")
+// let B = +prompt("2-Sonni kiriting..")
+// let C = +prompt("3-Sonni kiriting..")
+
+// let =   res = false;
+
+// if (A < 0 && B > 0 && C > 0 || A > 0 && B < 0 && C > 0 || A > 0 && B > 0 && C < 0 ){
+//     res = true;
+// }
+// console.log(res)
+
+// 16-misol
+
+// let A = +prompt("1-Sonni kiriting..")
+// let B = +prompt("2-Sonni kiriting..")
+// let C = +prompt("3-Sonni kiriting..")
+
+// let =   res = false;
+
+// if (A < 0 && B > 0 && C > 0 || A > 0 && B < 0 && C > 0 || A > 0 && B > 0 && C < 0 ){
+//     res = true;
+// }
+// console.log(res)
+
+
+// Ternary operatoriga oid masalalar
+
+// 1-masala
+
+// let a = +prompt("Harorratni kiriting..")
+
+//     a > 25 ? console.log("Issiq") :  console.log("Sovuq")
+
+// 2-masala
+
+// let a = +prompt("Imtihon balini kiriting..")
+
+//     a >= 50 ? console.log("Imtihondan otdi") :  console.log("Imtihondan yiqildi")
+
+// 3-masala
+
+// let a = +prompt("Son kiriting...")
+
+//     a > 0 ? console.log("Musbat son") :  console.log("Manfiy son")
+
+
+
+
+// For sikl operatoriga oid masalalar
+
+// 1-masala
+
+
+
+// for(let i = 1; i <= 10; i++) {
+//     console.log(i)
+// }
+
+// 2-masala
+
+// for(let i = 10; i >= 1; i--) {
+//     console.log(i)
+// }
+
+// 3-masala
+
+
+// for(let i = 1; i <= 100; i++) {
+//    if(i % 2 == 0) {
+//     console.log(i)
+//    }
+// }
+
+// 4-masala
+
+// for(let i = 1; i <= 100; i++) {
+//     if(i % 2 == 1) {
+//      console.log(i)
+//     }
+//  }
+
+// 5-masala
+// let res = 0;
+// for(let i = 1; i <= 10; i++) {
+//    res = res + i;
+//  }
+//  console.log(res);
+
+// 6-masala
+
+// let res = 1;
+// for(let i = 1; i <= 10; i++) {
+//    res = res * i;
+//  }
+//  console.log(res);
+
+// 8-masala
+
+
+// for(let i = 1; i <= 50; i++) {
+//     console.log(i * i);
+//  }
+
+// 9-masala
+
+// for(let i = 1; i <= 20; i++) {
+//     console.log(i * i * i);
+//  }
+
+// 10-masala
+// let sum = 0
+// for(let i = 1; i <= 1000; i++) {
+//     sum = sum + i
+//  }
+//  console.log(sum)
+
+// 11-masala
+
+// for (let i = 0; i < 5; i++) {
+//     console.log("Salom");
+// }
+
+// 12-masala
+
+// for (let i = 1; i <= 100; i++) {
+//     console.log(i * 2);
+// }
+ 
+
+
+
